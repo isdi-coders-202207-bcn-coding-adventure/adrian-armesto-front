cba98f9b36b5e7721442a0562bc05346f7eef28b
Adrian Armesto <armestomena@gmail.com>
2022-08-22 16:47:29 +0200

    Nest selectors

--- src/components/Counter/CounterStyle.tsx
+++ src/components/Counter/CounterStyle.tsx
@@ -8,29 +8,30 @@ const CounterStyle = styled.section`
   justify-content: center;
   align-items: center;
   padding: 5%;
-
-  .counter__counter {
-    margin: 10px;
-    width: 300px;
-    height: 300px;
-    border-radius: 25px;
-    background-color: #fac204;
-    color: #243444;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: 5em;
-  }
-  .counter__container {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  }
-  .counter__text {
-    color: white;
-    justify-self: center;
-    font-size: 3em;
+  .counter {
+    &__counter {
+      margin: 10px;
+      width: 300px;
+      height: 300px;
+      border-radius: 25px;
+      background-color: #fac204;
+      color: #243444;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 5em;
+    }
+    &__container {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+    }
+    &__text {
+      color: white;
+      justify-self: center;
+      font-size: 3em;
+    }
   }
 `;
 

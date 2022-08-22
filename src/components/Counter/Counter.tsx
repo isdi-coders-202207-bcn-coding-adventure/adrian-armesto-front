12d819b2774b30db29e6df8f3546703ea3d1f3a9
Adrian Armesto <armestomena@gmail.com>
2022-08-22 15:45:53 +0200

    Change h2 for spans and change the className

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -3,19 +3,19 @@ const Counter = () => {
     <section className="counter">
       <div className="counter__container">
         <div className="counter__counter">02</div>
-        <h2 className="counter__title">days</h2>
+        <span className="counter__text">days</span>
       </div>
       <div className="counter__container">
         <div className="counter__counter">20</div>
-        <h2 className="counter__title">hours</h2>
+        <span className="counter__text">hours</span>
       </div>
       <div className="counter__container">
         <div className="counter__counter">34</div>
-        <h2 className="counter__title">minutes</h2>
+        <span className="counter__text">minutes</span>
       </div>
       <div className="counter__container">
         <div className="counter__counter">09</div>
-        <h2 className="counter__title">seconds</h2>
+        <span className="counter__text">seconds</span>
       </div>
     </section>
   );

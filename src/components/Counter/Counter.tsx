d483148a0d3f5ebdd9e328924f27e8c7c0a06eeb
Adrian Armesto <armestomena@gmail.com>
2022-08-22 15:23:02 +0200

    Added h2 tag to titles and delete spans on divs

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -2,28 +2,20 @@ const Counter = () => {
   return (
     <section className="counter">
       <div className="counter__container">
-        <div className="counter__counter">
-          <span>02</span>
-        </div>
-        <span className="counter__title">days</span>
+        <div className="counter__counter">02</div>
+        <h2 className="counter__title">days</h2>
       </div>
       <div className="counter__container">
-        <div className="counter__counter">
-          <span>20</span>
-        </div>
-        <span className="counter__title">hours</span>
+        <div className="counter__counter">20</div>
+        <h2 className="counter__title">hours</h2>
       </div>
       <div className="counter__container">
-        <div className="counter__counter">
-          <span>34</span>
-        </div>
-        <span className="counter__title">minutes</span>
+        <div className="counter__counter">34</div>
+        <h2 className="counter__title">minutes</h2>
       </div>
       <div className="counter__container">
-        <div className="counter__counter">
-          <span>09</span>
-        </div>
-        <span className="counter__title">seconds</span>
+        <div className="counter__counter">09</div>
+        <h2 className="counter__title">seconds</h2>
       </div>
     </section>
   );

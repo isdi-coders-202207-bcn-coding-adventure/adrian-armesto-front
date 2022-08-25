7fd5911f8ded454e292c2a0a44386b676ec4ac1d
Adrian Armesto <armestomena@gmail.com>
2022-08-25 13:17:39 +0200

    Add description to tests

--- src/hooks/useCountdown.test.ts
+++ src/hooks/useCountdown.test.ts
@@ -7,9 +7,9 @@ const mockDate = [0, 1, 2, 3, 4];
 
 jest.mock("../utils/timeCalculate", () => () => mockDate);
 
-describe("s", () => {
-  describe("s", () => {
-    test("s", () => {
+describe("Given a useCounDown custom hook", () => {
+  describe("When it is calles", () => {
+    test("Then it should call de setStat of the use satet with the coun dwon date les the actual date", () => {
       const setStateMock = jest.fn();
       const useStateMock: any = (useState: any) => [useState, setStateMock];
       jest.spyOn(React, "useState").mockImplementation(useStateMock);
@@ -24,7 +24,7 @@ describe("s", () => {
       expect(setStateMock).toHaveBeenCalledWith(finalDate);
     });
 
-    test("f", () => {
+    test("Then it should return the return value of time calculate function", () => {
       const setStateMock = jest.fn();
       const useStateMock: any = (useState: any) => [useState, setStateMock];
       jest.spyOn(React, "useState").mockImplementation(useStateMock);
